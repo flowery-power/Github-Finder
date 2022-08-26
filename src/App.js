a117f5e4d33fa5d59c0873fcc0c4c3652b1f4196
flowery-power <gabrovska.tsvetelina@gmail.com>
2022-08-26 13:04:03 +0300

    set up github context commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/layout/Footer";
 
-import Home from "./pages/Home.js";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className=".flex.flex-col.justify-between.h-screen">
-        <Navbar />
-      </div>
+    <GithubProvider>
+      <BrowserRouter>
+        <div className=".flex.flex-col.justify-between.h-screen">
+          <Navbar />
+        </div>
 
-      <main className="container mx-auto px-3 pb-12">Content</main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/notfound" element={<NotFound />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+        <main className="container mx-auto px-3 pb-12">Content</main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
 
-      <Footer />
-    </BrowserRouter>
+        <Footer />
+      </BrowserRouter>
+    </GithubProvider>
   );
 }
 
